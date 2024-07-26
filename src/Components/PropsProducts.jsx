a371619d990049cf6ba1cd2667695bddbc4eb121
QUY666666 <lenhatquy2401@gmail.com>
2024-07-26 09:42:42 +0700

    update n

--- src/Components/PropsProducts.jsx
+++ src/Components/PropsProducts.jsx
@@ -8,8 +8,36 @@ const PropsProducts = ({item}) => {
         <p>Giá {item.price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
                         {/* <p>Còn: {item.quantity}</p> */}
         <p></p>
-        <button className=''>Xem chi tiết</button>
+        <div className="d-flex justify-content-center">
+        {/* <button className=''>Xem chi tiết</button> */}
+        {/* modal */}
+        {/* <!-- Button trigger modal --> */}
+<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Xem Chi Tiết
+</button>
 
+
+{/* <!-- Modal --> */}
+<div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog modal-lg">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h1 className="modal-title fs-5" id="exampleModalLabel">Chi Tiết Sản Phẩm</h1>
+        {/* <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" /> */}
+      </div>
+      <div className="modal-body">
+        ...
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        {/* <button type="button" className="btn btn-primary">Save changes</button> */}
+      </div>
+    </div>
+  </div>
+</div>
+
+        {/* modal */}
+        </div>
     </div>
   )
 }
